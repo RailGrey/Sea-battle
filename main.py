1f56c6c6910bb2c405e05d4f074aaaec832f390c
Rail Khashaev <khashaev.rm@phystech.edu>
2021-12-11 22:44:42 +0300

    Minor

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ WIDTH = 1000
 
 # window with game, rectangle(left up angle cors, width, height)
 game_window = (0, 0, 800, 800)
-FPS = 15
+FPS = 7
 
 
 grid_lenght = int(input('Введите длину поля: '))
